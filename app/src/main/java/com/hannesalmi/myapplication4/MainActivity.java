302ff9bbe24283a17579d243996ead1c74cf0d1a
hannemsalmi <hanne.salmi@metropolia.fi>
2021-11-29 11:32:11 +0200

    Initial commit

---
+++ app/src/main/java/com/hannesalmi/myapplication4/MainActivity.java
@@ -0,0 +1,16 @@
+package com.hannesalmi.myapplication4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
